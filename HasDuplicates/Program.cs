25686094b66d3cf25ae6b81ba862a8aef3397552
Kelcey Korngay <kelcey.kornegay@gmail.com>
2018-07-01 14:14:35 -0400

    commit
    chk for dups in a array

--- HasDuplicates/Program.cs
+++ HasDuplicates/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//chk for dups in a array
 namespace HasDuplicates
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var dups = new string[5] { "mike","dave","mike", "kim", "dave" };
+            HasDuplicates.HasDuplicate(dups);
+        }
+    }
+    public class HasDuplicates
+    {
+        public static void HasDuplicate(string[] dups)
+        {
+            List<string> chk = new List<string>();
+
+            for (int i = 0; i < dups.Length; i++)
+            {
+                if(chk.Contains(dups[i]))
+                {
+                    Console.WriteLine("Dup {0}", dups[i]);
+                }
+                else
+                {
+                    chk.Add(dups[i]);
+                }
+                //Console.WriteLine("\n{0}",dups[i]);
+
+            }
         }
     }
 }
